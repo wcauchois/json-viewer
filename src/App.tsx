935ff964f8c37a6cc6cdf5d5f382d018af393701
Bill Cauchois <wcauchois@gmail.com>
2023-10-29 21:30:54 -0400

    decent toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx"
 import { useText } from "./state"
 import { Tabs } from "./system/Tabs"
 import { ReactNode, useRef } from "react"
+import React from "react"
 
 function ViewerTab(props: { className?: string }) {
 	const { className } = props
@@ -21,6 +22,29 @@ function Button(props: { children: ReactNode; onClick?: () => void }) {
 	)
 }
 
+interface ToolbarItem {
+	name: string
+	action: () => void
+}
+
+function Toolbar(props: { itemGroups: ToolbarItem[][] }) {
+	const { itemGroups } = props
+
+	return (
+		<div className="flex px-2 py-1 border-b divide-x">
+			{itemGroups.map((group, i) => (
+				<div className="flex gap-2 px-2 first:pl-0" key={i}>
+					{group.map((item, j) => (
+						<Button key={j} onClick={item.action}>
+							{item.name}
+						</Button>
+					))}
+				</div>
+			))}
+		</div>
+	)
+}
+
 function TextTab(props: { className?: string }) {
 	const { className } = props
 
@@ -31,37 +55,61 @@ function TextTab(props: { className?: string }) {
 
 	return (
 		<div className={clsx(className, "w-full h-full flex flex-col")}>
-			<div className="flex gap-2 px-2 py-1 border-b">
-				<Button
-					onClick={() => {
-						const parsed = useText.getState().parsed
-						if (parsed.type === "success") {
-							setText(JSON.stringify(parsed.value, null, 2))
-							textareaRef.current?.scrollTo(0, 0)
-						}
-					}}
-				>
-					Format
-				</Button>
-				<Button
-					onClick={() => {
-						const parsed = useText.getState().parsed
-						if (parsed.type === "success") {
-							setText(JSON.stringify(parsed.value))
-							textareaRef.current?.scrollTo(0, 0)
-						}
-					}}
-				>
-					Remove whitespace
-				</Button>
-				<Button
-					onClick={() => {
-						setText("")
-					}}
-				>
-					Clear
-				</Button>
-			</div>
+			<Toolbar
+				itemGroups={[
+					[
+						{
+							name: "Paste",
+							action: async () => {
+								if (!navigator.clipboard) {
+									return
+								}
+								const clipboardText = await navigator.clipboard.readText()
+								setText(clipboardText)
+							},
+						},
+						{
+							name: "Copy",
+							action: async () => {
+								if (!navigator.clipboard) {
+									return
+								}
+								navigator.clipboard.writeText(text)
+							},
+						},
+					],
+					[
+						{
+							name: "Format",
+							action: () => {
+								const parsed = useText.getState().parsed
+								if (parsed.type === "success") {
+									setText(JSON.stringify(parsed.value, null, 2))
+									textareaRef.current?.scrollTo(0, 0)
+								}
+							},
+						},
+						{
+							name: "Remove whitespace",
+							action: () => {
+								const parsed = useText.getState().parsed
+								if (parsed.type === "success") {
+									setText(JSON.stringify(parsed.value))
+									textareaRef.current?.scrollTo(0, 0)
+								}
+							},
+						},
+					],
+					[
+						{
+							name: "Clear",
+							action: () => {
+								setText("")
+							},
+						},
+					],
+				]}
+			/>
 			<textarea
 				ref={textareaRef}
 				className="p-1 text-xs font-mono resize-none grow"
