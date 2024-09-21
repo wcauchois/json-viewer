e39d1c819a191f0200df36eb396c7beac79574ff
Bill Cauchois <wcauchois@gmail.com>
2024-09-21 16:52:54 -0400

    default expand on paste

--- src/lib/appActions.ts
+++ src/lib/appActions.ts
@@ -1,10 +1,20 @@
 import { useAppState } from "../state/app"
 import { showSnackbar } from "../state/snackbar"
 import { checkpointStore } from "./CheckpointStore"
+import { flattenAST } from "./jsonAst"
 
 export async function pasteFromClipboard() {
 	const clipboardText = await navigator.clipboard?.readText()
 	useAppState.getState().setText(clipboardText ?? "")
+
+	const updatedAppState = useAppState.getState()
+	if (updatedAppState.parseResult.type === "success") {
+		updatedAppState.setNodesExpanded(
+			flattenAST(updatedAppState.parseResult.value.ast),
+			true
+		)
+	}
+
 	if (clipboardText) {
 		checkpointStore.upsertCheckpoint({
 			content: clipboardText,
