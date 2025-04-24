bd2190e1c84a52423ede9d27b0ce2e39bd346044
Bill Cauchois <wcauchois@gmail.com>
2025-04-23 20:34:04 -0400

    decompress too

--- src/lib/routing.tsx
+++ src/lib/routing.tsx
@@ -1,27 +1,69 @@
 import { ReactNode, useEffect } from "react"
 import { useAppState } from "../state/app"
-import { makeDeferred, useOnInitialMount } from "./utils"
+import { isDefined, makeDeferred, useOnInitialMount } from "./utils"
 import { worker } from "../worker/workerClient"
 
 // Some stuff cribbed from: https://github.com/topaz/paste/blob/master/index.html
 
 export function forceCommitToHash() {
-	// TODO: Do I need this?
+	const { text } = useAppState.getState()
+	setHashFromText(text, { saveBeforeReplacing: true })
 }
 
-function getTextFromHash() {
-	const hashContents = location.hash.replace(/^#\//, "")
-	return hashContents
+async function getTextFromHash() {
+	const base64 = location.hash.replace(/^#\//, "")
+	if (base64.length === 0) {
+		return
+	}
+
+	try {
+		// Base64 -> Uint8Array
+		const response = await fetch(
+			"data:application/octet-stream;base64," + base64
+		)
+		const blob = await response.blob()
+
+		const readerLoadDeferred = makeDeferred<undefined>()
+		const reader = new FileReader()
+		reader.onload = () => {
+			readerLoadDeferred.resolve(undefined)
+		}
+		reader.readAsArrayBuffer(blob)
+		await readerLoadDeferred.promise
+
+		if (!reader.result) {
+			return
+		}
+
+		if (typeof reader.result === "string") {
+			throw new Error(`FileReader: did not expect string result`)
+		}
+		const compressed = new Uint8Array(reader.result)
+
+		// Decompress
+		const decompressed = await worker.decompress(compressed)
+
+		// Uint8Array -> String
+		const decoder = new TextDecoder()
+		return decoder.decode(decompressed)
+	} catch (err) {
+		console.error("Error converting hash:", err)
+		return
+	}
 }
 
-async function setHashFromText(text: string) {
-	const url = new URL(window.location.href)
-	console.log("set hash from text")
-
+async function setHashFromText(
+	text: string,
+	options: { saveBeforeReplacing?: boolean } = {}
+) {
+	// String -> Uint8Array
 	const encoder = new TextEncoder()
 	const encoded = encoder.encode(text)
+
+	// Compress
 	const compressed = await worker.compress(encoded)
 
+	// Uint8Array -> Base64
 	const readerLoadDeferred = makeDeferred<undefined>()
 	const reader = new FileReader()
 	reader.onload = () => {
@@ -36,17 +78,29 @@ async function setHashFromText(text: string) {
 
 	const base64 = reader.result.substring(reader.result.indexOf(",") + 1)
 
-	url.hash = `#/${base64}`
-	window.history.replaceState(null, "", url)
+	if (options.saveBeforeReplacing) {
+		// XX: not working?
+		window.history.pushState(
+			new Date().toISOString(),
+			"",
+			new URL(window.location.href)
+		)
+	}
+
+	const nextUrl = new URL(window.location.href)
+	nextUrl.hash = `#/${base64}`
+	window.history.replaceState(new Date().toISOString(), "", nextUrl)
 }
 
 function useSyncRouteToState() {
-	const text = useAppState(state => state.text)
 	const setText = useAppState(state => state.setText)
 
 	// Sync state from hash on startup.
-	useOnInitialMount(() => {
-		//
+	useOnInitialMount(async () => {
+		const textFromHash = await getTextFromHash()
+		if (isDefined(textFromHash)) {
+			setText(textFromHash)
+		}
 	})
 
 	// Sync state to hash on state change.
@@ -60,8 +114,11 @@ function useSyncRouteToState() {
 
 	// Sync hash to state on hash change.
 	useEffect(() => {
-		function listener() {
-			setText(getTextFromHash())
+		async function listener() {
+			const textFromHash = await getTextFromHash()
+			if (isDefined(textFromHash)) {
+				setText(textFromHash)
+			}
 		}
 		window.addEventListener("hashchange", listener)
 		return () => window.removeEventListener("hashchange", listener)
--- src/worker/workerEntrypoint.ts
+++ src/worker/workerEntrypoint.ts
@@ -1,18 +1,22 @@
 import * as Comlink from "comlink"
-import init, { compress } from "lzma-rs"
+import init, { compress, decompress } from "lzma-rs"
 import wasmUrl from "lzma-rs/lzma_rs_bg.wasm?url"
 
-console.log("wasm url", wasmUrl)
-const lzmaInitialized = init(wasmUrl)
+const lzmaInitialized = init({ module_or_path: wasmUrl })
 
 const workerApi = {
 	async ping() {
-		await lzmaInitialized
 		return "hello from worker"
 	},
-	compress: async (input: Uint8Array): Promise<Uint8Array> => {
+
+	async compress(input: Uint8Array): Promise<Uint8Array> {
 		await lzmaInitialized
 		return compress(input)
+	},
+
+	async decompress(input: Uint8Array): Promise<Uint8Array> {
+		await lzmaInitialized
+		return decompress(input)
 	},
 }
 
